3b3fb7cec76595c8217b3bead3818993bccf1864
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-10-06 19:09:09 -0300

    Criação de um estilo global (global.scss)
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import "./styles/global.scss";
+
 import HomePage from "./pages/home";
 
 function App() {
