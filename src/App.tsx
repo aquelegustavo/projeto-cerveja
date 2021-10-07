9057f73690853d8da5acb142e77d434c56a3162d
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-10-07 00:18:26 -0300

    Criação do loader (spin)
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import "./styles/global.scss";
 
 import HomePage from "./pages/home";
-import SignIn from "./auth/signin";
+
 import SignOut from "./auth/signout";
 
 import Profile from "./pages/profile";
@@ -12,21 +12,20 @@ import Profile from "./pages/profile";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import ProtectedRoute from "./auth/protected-route";
+import Loading from "./components/loading";
 
 function App() {
   const { isLoading } = useAuth0();
 
   if (isLoading) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <ProtectedRoute path="/profile" component={Profile} />
-
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signout" component={SignOut} />
+      <Route path="/logout" component={SignOut} />
     </Switch>
   );
 }
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 import styles from "./loading.module.scss";
 
-function LoadingComponent() {
-  return <h1 className={styles.title}>Carregando...</h1>;
+function Loading() {
+  return (
+    <div className={styles.container}>
+      <svg className={styles.loader} viewBox="0 0 24 24">
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+        <circle className={styles.loader__value} cx="12" cy="12" r="10" />
+      </svg>
+    </div>
+  );
 }
 
-export default LoadingComponent;
+export default Loading;
